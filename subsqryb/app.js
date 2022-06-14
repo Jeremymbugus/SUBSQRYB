cfa6fa7d2913ff5470a854ca26d3312fb1342a56
jeremy_stech <jeremiahm@mindtraclab.com>
2022-06-14 13:48:31 +0300

    Moved all apis to the routes index file from the app.js

--- subsqryb/app.js
+++ subsqryb/app.js
@@ -4,28 +4,17 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+const db = 'mongodb://localhost:27017/users'
 
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-var Stripe = require('./src/connect/stripe')
-var session = require('express-session')
-var MemoryStore = require('memorystore')(session)
+
+
 var mongoose = require('mongoose')
-const UserService = require('./src/user')
-
-
-const productToPriceMap = {
-    BASIC: 'price_1LAUIHGcTohqT9qay5ShZf4d',
-    PRO: 'price_1LAUK8GcTohqT9qaThRmydan'
-}
 
 var app = express();
-
-
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -44,100 +33,6 @@ app.use("/webhook", bodyParser.raw({ type: "application/json" }))
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
-
-//storing users data in session
-app.use(session({
-    saveUninitialized: false,
-    cookie: { maxAge: 86400000 },
-    store: new MemoryStore({
-        checkPeriod: 86400000
-    }),
-    resave: false,
-    secret: 'keyboard cat'
-}))
-
-app.post('/login', async (req, res) => {
-    const { email } = req.body
-    console.log("email", email)
-
-  let customer = await UserService.getUserByEmail(email)
-    let customerInfo = {}
-
-  if (!customer) {
-      console.log(`email ${email} does not exist. Making one.`)
-      try {
-          customerInfo = await Stripe.addNewCustomer(email)
-
-      customer = await UserService.addUser({
-          email: customerInfo.email,
-          billingID: customerInfo.id,
-          hasTrial: false,
-          plan: "none",
-          endDate: null
-      })
-
-      console.log(
-          `A new user signed up and addded to DB. The ID for ${email} is ${JSON.stringify(
-              customerInfo
-          )}`
-      )
-
-      console.log(`User also added to DB. Information from DB: ${customer}`)
-      } catch (e) {
-          console.log(e)
-          res.status(200).json({ e })
-          return
-      }
-  }
-
-  req.session.email = email
-    res.redirect('/account')
-})
-
-app.get('/account', async function (req, res) {
-    const { email } = req.session
-    const customer = await UserService.getUserByEmail(email)
-
-    if (!customer) {
-        res.redirect('/')
-    } else {
-        res.render('account.ejs', { customer })
-    }
-
-})
-
-app.post('/checkout', async (req, res) => {
-    const { customerID, product } = req.body
-
-  const price = productToPriceMap[product.toUpperCase()]
-    const session = await Stripe.createCheckoutSession(customerID, price)
-
-  res.send({ sessionId: session.id })
-})
-
-app.get('/success', (req, res) => {
-    res.send('Payment successful')
-})
-
-app.get('/failed', (req, res) => {
-    res.send('Payment failed')
-})
-
-app.post('/webhook', async (req, res) => {
-    let event
-
-  try {
-      event = Stripe.createWebhook(req.body, req.header('Stripe-Signature'))
-  } catch (err) {
-      console.log(err)
-      return res.sendStatus(400)
-  }
-
-  const data = event.data.object
-    console.log(event.type, data)
-
-  res.sendStatus(200)
-})
 
 
 /// catch 404 and forwarding to error handler
@@ -170,8 +65,8 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
+//Db Conncetion
 
-const db = 'mongodb://localhost:27017/users'
 mongoose.connect(
     db,
     {
@@ -184,8 +79,5 @@ mongoose.connect(
         if (error) console.log(error)
     }
 )
-
-
-
 
 module.exports = app;
--- subsqryb/routes/index.js
+++ subsqryb/routes/index.js
@@ -1,12 +1,124 @@
 var express = require('express');
+const Stripe = require("../src/connect/stripe");
 var router = express.Router();
-
+var session = require('express-session')
+var UserService = require('../src/user')
+var session = require('express-session')
+var MemoryStore = require('memorystore')(session)
+var productToPriceMap = {
+    BASIC: 'price_1LAUIHGcTohqT9qay5ShZf4d',
+    PRO: 'price_1LAUK8GcTohqT9qaThRmydan'
+}
 /* GET home page. */
 router.get('/', function (req, res) {
     res.render('login', {title: 'Subsqrb'});
     //res.status(200).render('login.ejs')
 });
 
+//storing users data in session
+router.use(session({
+    saveUninitialized: false,
+    cookie: { maxAge: 86400000 },
+    store: new MemoryStore({
+        checkPeriod: 86400000
+    }),
+    resave: false,
+    secret: 'keyboard cat'
+}))
+
+router.post('/login', async (req, res) => {
+    try{
+        const { email }  = req.body
+        console.log("email", email)
+        let customer = await UserService.getUserByEmail(email)
+
+        let customerInfo = {}
+
+        if (!customer) {
+            console.log(`email ${email} does not exist. Making one.`)
+            try {
+                customerInfo = await Stripe.addNewCustomer(email)
+                customer = await UserService.addUser({
+                    email: customerInfo.email,
+                    billingID: customerInfo.id,
+                    hasTrial: false,
+                    plan: "none",
+                    endDate: null
+                })
+
+                console.log(
+                    `A new user signed up and added to DB. The ID for ${email} is ${JSON.stringify(
+                        customerInfo
+                    )}`
+                )
+
+                console.log(`User also added to DB. Information from DB: ${customer}`)
+            } catch (e) {
+                console.log(e)
+                res.redirect('/')
+               // res.status(200).json({ e })
+                return
+            }
+        }
+        //console.log("email", email)
+        req.session.email = email
+        res.redirect('/account')
+    }catch (e) {
+        console.log(e)
+        res.redirect('/')
+        //res.status(200).json({ e })
+        return
+    }
+
+})
+
+router.get('/account', async function (req, res) {
+    const { email } = req.session
+    console.log('email' ,email)
+    const customer = await UserService.getUserByEmail(email)
+    console.log('jereny');
+    console.log(customer)
+    if (!customer) {
+        res.redirect('/')
+    } else {
+        res.render('account.ejs', { customer })
+    }
+
+
+})
+
+router.post('/checkout', async (req, res) => {
+    const { customerID, product } = req.body
+
+    const price = productToPriceMap[product.toUpperCase()]
+    const session = await Stripe.createCheckoutSession(customerID, price)
+
+    res.send({ sessionId: session.id })
+})
+
+router.get('/success', (req, res) => {
+    res.send('Payment successful')
+})
+
+router.get('/failed', (req, res) => {
+    res.send('Payment failed')
+})
+
+router.post('/webhook', async (req, res) => {
+    let event
+
+    try {
+        event = Stripe.createWebhook(req.body, req.header('Stripe-Signature'))
+    } catch (err) {
+        console.log(err)
+        return res.sendStatus(400)
+    }
+
+    const data = event.data.object
+    console.log(event.type, data)
+
+    res.sendStatus(200)
+})
 
 
 module.exports = router;
